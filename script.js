09ff945d5ab0f710b026d8295180358da3890030
MasonTren2 <92185361+MasonTren2@users.noreply.github.com>
2024-02-06 11:38:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showEventDetails() {
+    var eventDetails = document.getElementById('event-details');
+    if (eventDetails.style.display === 'none') {
+        eventDetails.style.display = 'block';
+    } else {
+        eventDetails.style.display = 'none';
+    }
+}
